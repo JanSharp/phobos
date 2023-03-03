f3dc7860e0a901ef3b21c7237a68144e583212dd
JanSharp <steinmeister36@gmail.com>
2023-03-03 14:01:36 +0100

    Add `util.iterate_keys`
    See previous commit message to understand the note about type inference

--- src/util.lua
+++ src/util.lua
@@ -156,6 +156,21 @@ local function replace_range(target, range, start_index, stop_index, range_start
   end
 end
 
+---must use the letter K for the generic type parameter, otherwise it won't infer the type of the key (in 3.6.13)
+---@generic K
+---@param tab table<K, any>
+---@param start_key? K
+---@return fun(_: any? ,_: K?):(K?) iterator @
+---NOTE: doesn't actually take any parameters, just works around an issue with type inference (in 3.6.13)
+local function iterate_keys(tab, start_key)
+  local key = start_key
+  return function()
+    local result = key
+    key = next(tab, key)
+    return result
+  end
+end
+
 local function debug_abort(message)
   return error(message)
 end
@@ -329,6 +344,7 @@ return {
   insert_range = insert_range,
   remove_range = remove_range,
   replace_range = replace_range,
+  iterate_keys = iterate_keys,
   debug_abort = debug_abort,
   abort = abort,
   debug_assert = debug_assert,
