JanSharp/phobos

commit 89491905eb4d1196a6792a4c5b0e2df2ded8a69f

Add `get_insts` to real live reg ranges
Add a few more tests

src/il_real_liveliness.lua
@@ -47,9 +47,8 @@ local function merge_open_blocks(data, partially_open_block, regs_to_add)
     local visited_reg_range = visited_lut[reg_range.reg]
     if visited_reg_range then
       if reg_range ~= visited_reg_range then
-        reg_range.set_insts = linq(visited_reg_range.set_insts)
-          :union(reg_range.set_insts)
-          :to_array()
+        reg_range.set_insts = linq(visited_reg_range.set_insts):union(reg_range.set_insts):to_array()
+        reg_range.get_insts = linq(visited_reg_range.get_insts):union(reg_range.get_insts):to_array()
         data.renamed_live_reg_ranges[visited_reg_range] = reg_range
       end
     else
@@ -73,7 +72,9 @@ local function add_to_real_live_regs(execution_checkpoint, real_live_regs)
       list[#list+1] = live_reg
       lut[live_reg.reg] = live_reg
     else
-      util.debug_abort("Attempt to add a live reg range for the same register twice.")
+      util.debug_abort("Attempt to add a live reg range for the same register at the same execution \z
+        checkpoint twice."
+      )
     end
   end
 end
@@ -220,18 +221,26 @@ local function eval_live_regs_in_block(data, open_block)
       visit_regs_for_inst_deduplicated(data, inst, function(_, _, reg, get_set)
         if reg.is_internal then return end
         local live_reg_range = regs_lut[reg]
-        if bit32.band(get_set, set_flag) ~= 0 and regs_lut[reg] then
-          if not linq(live_reg_range.set_insts):contains(inst) then
-            live_reg_range.set_insts[#live_reg_range.set_insts+1] = inst
-          end
+        if bit32.band(get_set, set_flag) ~= 0 and live_reg_range then
+          live_reg_range.set_insts[#live_reg_range.set_insts+1] = inst
           regs_lut[reg] = nil
           util.remove_from_array_fast(regs_waiting_for_set, live_reg_range)
         end
-        if open_block.is_first_pass and bit32.band(get_set, get_flag) ~= 0 and not regs_lut[reg] then
-          -- Don't create new live reg ranges in additional passes, they'd be duplicates.
-          live_reg_range = {reg = reg, set_insts = {}}
-          regs_lut[reg] = live_reg_range
-          regs_waiting_for_set[#regs_waiting_for_set+1] = live_reg_range
+        if bit32.band(get_set, get_flag) ~= 0 then
+          if open_block.is_first_pass and not live_reg_range then
+            -- Don't create new live reg ranges in additional passes, they'd be duplicates.
+            live_reg_range = {reg = reg, set_insts = {}, get_insts = {}}
+            regs_lut[reg] = live_reg_range
+            regs_waiting_for_set[#regs_waiting_for_set+1] = live_reg_range
+          end
+          if not open_block.is_first_pass and live_reg_range then
+            regs_lut[reg] = nil
+            util.remove_from_array_fast(regs_waiting_for_set, live_reg_range)
+            live_reg_range = nil ---@diagnostic disable-line: cast-local-type
+          end
+          if live_reg_range then
+            live_reg_range.get_insts[#live_reg_range.get_insts+1] = inst
+          end
         end
 
         -- Keep track of which instructions use a register which is captured as an upvalue, get and set.
@@ -241,6 +250,9 @@ local function eval_live_regs_in_block(data, open_block)
       end)
     end
 
+    if not open_block.is_first_pass and not regs_waiting_for_set[1] then
+      break
+    end
     if inst ~= open_block.block.start_inst then
       add_to_real_live_regs(inst.prev_border, regs_waiting_for_set)
     end
@@ -396,11 +408,9 @@ local function upvals_are_extra_special(data)
       local checkpoint = open.checkpoints[i]
       local existing_live_range = checkpoint.live_range_by_reg[open.reg]
       if existing_live_range then
-        if existing_live_range.is_captured_as_upval then
-          open.live_range.set_insts = linq(open.live_range.set_insts)
-            :union(existing_live_range.set_insts)
-            :to_array()
-        end
+        local live_range = open.live_range
+        live_range.set_insts = linq(live_range.set_insts):union(existing_live_range.set_insts):to_array()
+        live_range.get_insts = linq(live_range.get_insts):union(existing_live_range.get_insts):to_array()
         util.remove_from_array_fast(checkpoint.real_live_regs, existing_live_range)
       end
       checkpoint.real_live_regs[#checkpoint.real_live_regs+1] = open.live_range

src/classes.lua
@@ -847,8 +847,13 @@
 ---Does this live register range get captured as an upvalue for an inner function?
 ---@field is_captured_as_upval boolean?
 ---The instructions setting/writing to this live reg range, the beginning(s) of its lifetime.\
----`nil` when `is_param` is `true`.
+---When `is_captured_as_upval` is `true` there can be multiple set_insts within the same block, otherwise not.\
+---`nil` when `is_param` is `true`, otherwise always contains at least 1 instruction.
 ---@field set_insts ILInstruction[]
+---The instructions getting/reading from this live reg range. All ends of the lifetime of the live reg range
+---are in `get_insts`, however not all `get_insts` are the end of its lifetime.\
+---Always contains at least 1 instruction.
+---@field get_insts ILInstruction[]
 ---
 ---@field forced_color integer @ 1 based.
 

src/il_register_coloring.lua
@@ -116,7 +116,7 @@ end
   inserted before the register list consuming instruction
 - [x] somehow force the color of parameter registers
 - [x] remember the instruction which set a live reg range
-- [ ] maybe remember all instructions which get a live reg range
+- [x] maybe remember all instructions which get a live reg range
 - [ ] evaluate all live reg ranges which are not at top and not apart of register lists first
 - [x] ignore internal regs
 - [ ] live reg ranges which outlive a register group in both directions must be below the register list

tests/test_il_real_liveliness.lua
@@ -10,8 +10,12 @@ local il_blocks = require("il_blocks")
 local ill = require("indexed_linked_list")
 local util = require("util")
 local il_pretty_print = require("il_pretty_print")
+local linq = require("linq")
 
 local il_real_liveliness = require("il_real_liveliness")
+
+-- NOTE: Using the tokenizer, parser and il generator to make ILFunctions for testing is bad. But this the
+-- easiest right now, because IL is still lacking utility functions to easily define the data structure.
 
 -- TODO: Add proper support for extra output to the test runner, or something.
 local output_pretty_printed = false
@@ -79,6 +83,17 @@ local function assert_same_factory()
   end
 end
 
+---@param checkpoint ILExecutionCheckpoint
+local function assert_no_live_regs(checkpoint)
+  assert.equals(nil, checkpoint.real_live_regs[1], "There should be 0 live reg ranges here.")
+end
+
+---@param insts ILInstruction[]
+---@return ILInstruction[]
+local function order_by_inst_index(insts)
+  return linq(insts):order_by(function(inst) return inst.index end):to_array()
+end
+
 ---@param source_code string
 ---@return ILFunction
 local function get_il(source_code)
@@ -142,7 +157,7 @@ do
       validate_result(il_func)
     end)
 
-    main_scope:add_test(name.." (has valid set_insts)", function()
+    main_scope:add_test(name.." (has valid set_insts and get_insts)", function()
       local il_func = func()
       if type(il_func) == "string" then
         il_func = get_il(il_func)
@@ -151,7 +166,7 @@ do
 
       local block = il_func.blocks.first
       ---@param checkpoint ILExecutionCheckpoint
-      local function assert_set_insts(checkpoint)
+      local function assert_set_and_get_insts(checkpoint)
         for _, live_range in ipairs(checkpoint.real_live_regs) do
           if live_range.is_param then
             assert.equals(nil, live_range.set_insts, "set_insts when is_param")
@@ -159,19 +174,21 @@ do
             assert.not_equals(nil, live_range.set_insts, "set_insts when not is_param")
             assert.not_equals(nil, live_range.set_insts[1], "set_insts[1] when not is_param")
           end
+          assert.not_equals(nil, live_range.get_insts, "get_insts")
+          assert.not_equals(nil, live_range.get_insts[1], "get_insts[1]")
         end
       end
       while block do
         for inst in il_blocks.iterate(il_func, block) do
           if inst ~= block.start_inst then
-            assert_set_insts(inst.prev_border)
+            assert_set_and_get_insts(inst.prev_border)
           end
         end
         if block.straight_link then
-          assert_set_insts(block.straight_link)
+          assert_set_and_get_insts(block.straight_link)
         end
         if block.jump_link then
-          assert_set_insts(block.jump_link)
+          assert_set_and_get_insts(block.jump_link)
         end
         block = block.next
       end
@@ -191,6 +208,37 @@ do
     end
   end)
 
+  add_test("The same reg gets a single live range in a loop block", function()
+    return [[
+      local foo = 100
+      while true do
+        local _ = foo
+      end
+    ]], function(func)
+      local assert_same = assert_same_factory()
+      assert_same(get_inst(func, 1).block.straight_link)
+      assert_same(get_inst(func, 2).next_border)
+      assert_same(get_inst(func, 3).block.straight_link)
+      assert_same(get_inst(func, 4).next_border)
+      assert_same(get_inst(func, 5).block.jump_link)
+      assert_no_live_regs(get_inst(func, 6).next_border)
+    end
+  end)
+
+  add_test("Multiple get_insts for one live reg range", function()
+    return [[
+      local foo = 100
+      local _ = foo
+      return foo
+    ]], function(func)
+      local reg_range = get_inst(func, 1).next_border.real_live_regs[1]
+      local get_insts = order_by_inst_index(reg_range.get_insts)
+      assert.equals(get_inst(func, 2), get_insts[1])
+      assert.equals(get_inst(func, 3), get_insts[2])
+      assert.equals(nil, get_insts[3], "get_insts[3] == nil")
+    end
+  end)
+
   add_test("Multiple set_insts for one live reg range", function()
     return [[
       local foo
@@ -201,11 +249,59 @@ do
       end
       return foo
     ]], function(func)
-      local reg_range = get_inst(func, 7).next_border.real_live_regs[1]
-      assert.not_equals(nil, reg_range.set_insts[1], "set_insts[1]")
-      assert.not_equals(nil, reg_range.set_insts[2], "set_insts[2]")
-      assert.equals(nil, reg_range.set_insts[3], "set_insts[3]")
-      assert.not_equals(reg_range.set_insts[1], reg_range.set_insts[2], "set_insts[1] ~= set_insts[2]")
+      local reg_range = get_inst(func, 3).next_border.real_live_regs[1]
+      local set_insts = order_by_inst_index(reg_range.set_insts)
+      assert.equals(get_inst(func, 3), set_insts[1], "set_insts[1] == inst 3")
+      assert.equals(get_inst(func, 6), set_insts[2], "set_insts[2] == inst 6")
+      assert.equals(nil, set_insts[3], "set_insts[3] == nil")
+    end
+  end)
+
+  add_test("Multiple get_insts for one live reg range with a loop", function()
+    return [[
+      local foo = 100
+      local _ = foo
+      local _ = foo
+      while true do
+        local _ = foo
+        local _ = foo
+      end
+    ]], function(func)
+      local reg_range = get_inst(func, 1).block.straight_link.real_live_regs[1]
+      local get_insts = order_by_inst_index(reg_range.get_insts)
+      assert.equals(get_inst(func, 2), get_insts[1], "get_insts[1] == inst 2")
+      assert.equals(get_inst(func, 3), get_insts[2], "get_insts[2] == inst 3")
+      assert.equals(get_inst(func, 6), get_insts[3], "get_insts[3] == inst 6")
+      assert.equals(get_inst(func, 7), get_insts[4], "get_insts[4] == inst 7")
+      assert.equals(nil, get_insts[5], "get_insts[5] == nil")
+    end
+  end)
+
+  add_test("Multiple set_insts for one live reg range with a loop", function()
+    return [[
+      local foo
+      if true then
+        foo = 100
+      else
+        foo = 200
+      end
+      while true do
+        if true then
+          foo = 300
+        end
+        if true then
+          foo = 400
+        end
+        local _ = foo
+      end
+    ]], function(func)
+      local reg_range = get_inst(func, 3).next_border.real_live_regs[1]
+      local set_insts = order_by_inst_index(reg_range.set_insts)
+      assert.equals(get_inst(func, 3), set_insts[1], "set_insts[1] == inst 3")
+      assert.equals(get_inst(func, 6), set_insts[2], "set_insts[2] == inst 6")
+      assert.equals(get_inst(func, 11), set_insts[3], "set_insts[3] == inst 11")
+      assert.equals(get_inst(func, 15), set_insts[4], "set_insts[4] == inst 15")
+      assert.equals(nil, set_insts[5], "set_insts[5] == nil")
     end
   end)
 

commit 8de0013a039518f0bbea761c2e727fd76eedeebb

Fix get set insts not splitting live reg ranges
This got broken in 89491905eb4d1196a6792a4c5b0e2df2ded8a69f

src/il_real_liveliness.lua
@@ -269,6 +269,7 @@ local function eval_live_regs_in_block(data, open_block)
           live_reg_range.set_insts[#live_reg_range.set_insts+1] = inst
           regs_lut[reg] = nil
           util.remove_from_array_fast(regs_waiting_for_set, live_reg_range)
+          live_reg_range = nil ---@diagnostic disable-line: cast-local-type
         end
         if bit32.band(get_set, get_flag) ~= 0 then
           if open_block.is_first_pass and not live_reg_range then